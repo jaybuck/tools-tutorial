b2a9d31c0589097ca260157ee7fbfc2778ee9a67
Jay Buckingham <jaybuckingham@gmail.com>
2016-11-01 15:05:19 -0700

    Initial C++ example

---
+++ cpp_example1/example1_main.cpp
@@ -0,0 +1,19 @@
+// example1_main.cpp
+// Example program for C++ tutorial
+//
+
+#include <string>
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main(const int argc, const char* argv[])
+{
+    int result = 0;
+    string program_name = argv[0];
+    
+    printf("%s started\n", program_name.c_str());
+
+    return result;
+}
